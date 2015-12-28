a5b4bcb211fb345081514420bf7ffbeefa46ffde
arneboe <github@rnae.de>
2015-12-28 19:00:14 +0100

    HardwareController: subscribe to server changes and update button colors accordingly

--- pixelcontroller-gui/src/main/java/com/neophob/sematrix/gui/HardwareController/HardwareControllerHandler.java
+++ pixelcontroller-gui/src/main/java/com/neophob/sematrix/gui/HardwareController/HardwareControllerHandler.java
@@ -1,33 +1,48 @@
 package com.neophob.sematrix.gui.HardwareController;
 
 import com.neophob.sematrix.core.properties.ValidCommand;
+import com.neophob.sematrix.gui.HardwareController.IHardwareController.HWButtonState;
 import com.neophob.sematrix.gui.service.PixConServer;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
 /**
- * Created by arne on 25.12.2015.
+ * Connects a HardwareController to the PixConServer.
+ * Handles two-way communication between the hardware and the PixConServer
  */
-public class HardwareControllerHandler implements IHardwareControllerSubscriber {
+public class HardwareControllerHandler implements IHardwareControllerSubscriber, Observer {
 
+    private static final int speedSlider = 0;
+    private static final int brightnessSlider = 1;
+    private static final  HWButtonState unusedPresetColor = HWButtonState.YELLOW_BLINK;
+    private static final HWButtonState usedPresetColor = HWButtonState.RED_BLINK;
+    private static final HWButtonState unusedRedColor = HWButtonState.RED_BLINK;
+    private static final HWButtonState unusedGreenColor = HWButtonState.RED_BLINK;
+    private final IHardwareController hw;
     private PixConServer server;
-    private static final IHardwareController.HWSlider speedSlider = IHardwareController.HWSlider.SL_0;
-    private static final IHardwareController.HWSlider brightnessSlider = IHardwareController.HWSlider.SL_1;
-    public HardwareControllerHandler(final PixConServer server) {
+
+    private int selectedPreset = 0; //the preset that is currently selected
+
+    public HardwareControllerHandler(final PixConServer server, IHardwareController hw) {
         this.server = server;
+        this.hw = hw;
+        initController();
+        hw.subscribe(this);
+        server.observeVisualState(this); //register to get updates if the user changes something in the gui manually
+        selectedPreset = server.getConfig().loadPresetOnStart();
+        displayPreset(selectedPreset);
     }
-
-    public void subscribeTo(IHardwareController ctrl)
-    {
-        ctrl.subscribe(this);
-    }
-
 
     @Override
     public void buttonPressed(int button) {
 
-        createMessage(ValidCommand.CHANGE_PRESET, buttonToPreset(button));
+        createMessage(ValidCommand.CHANGE_PRESET, button);
         sendMsg(ValidCommand.LOAD_PRESET);
-        server.refreshGuiState();
+        displayPreset(button);//highlight the button on the controller
+        //server.refreshGuiState();
     }
 
     @Override
@@ -35,13 +50,11 @@ public class HardwareControllerHandler implements IHardwareControllerSubscriber 
         if(slider == speedSlider) {
             final int val = map(newValue, 0, 127, 0, 200);
             createMessage(ValidCommand.GENERATOR_SPEED, val);
-            server.refreshGuiState();
         }
         else if(slider == brightnessSlider)
         {
             final int val = map(newValue, 0, 127, 0, 100);
             createMessage(ValidCommand.CHANGE_BRIGHTNESS, val);
-            server.refreshGuiState();
         }
         else
         {
@@ -63,22 +76,34 @@ public class HardwareControllerHandler implements IHardwareControllerSubscriber 
         server.sendMessage(msg);
     }
 
-    private int buttonToPreset(IHardwareController.HWButton button)
-    {
-        //this is tedious but fast
-        switch (button) {
-            case PB_0:
-                return 0;
-            case PB_1:
-                return 1;
-            case PB_2:
-                return 2;
-            case PB_3:
-                return 3;
-            default:
-                throw new NotImplementedException();
+    /**Sets all push button colors to the inital values */
+    private void initController() {
+        //FIXME blink is just for testing
+        for(int i = 0; i < 64; ++i) {//3 color push buttons
+            hw.setButtonState(i, unusedPresetColor);
+        }
+        for(int i = 64; i < 72; ++i) { //red push buttons
+            hw.setButtonState(i, unusedRedColor);
+        }
+        for(int i = 82; i < 90; ++i) { //green push buttons
+            hw.setButtonState(i, unusedGreenColor);
         }
     }
+
+    /**Display @p preset  */
+    private void displayPreset(final int preset) {
+        //turn old button off
+        //NOTE: selectedPreset can be -1 if the user did not specify a default preset in the config.properties
+        if(selectedPreset >= 0 && selectedPreset < 64) {
+            hw.setButtonState(selectedPreset, unusedPresetColor);
+        }
+        //turn new button on
+        if(preset >= 0 && preset < 64) {
+            hw.setButtonState(preset, usedPresetColor);
+        }
+        selectedPreset = preset;
+    }
+
 
     private int map(int x, int in_min, int in_max, int out_min, int out_max)
     {//http://stackoverflow.com/questions/7505991/arduino-map-equivalent-function-in-java
@@ -86,4 +111,31 @@ public class HardwareControllerHandler implements IHardwareControllerSubscriber 
     }
 
 
+    /**Is called whenever someone changes settings of the pixConServer  */
+    @Override
+    public void update(Observable o, Object arg) {
+        if (arg instanceof List<?>) {
+            for (Object obj : (List<?>) arg) {
+                String s = (String) obj;
+                String[] tmp = s.split(" ");
+                //we only care for brightness, speed and preset since that are the only things that can be controlled
+                //using the hardware controller right now
+                if(tmp[0].equals("CHANGE_PRESET")) {
+                    assert(tmp.length > 1);//protocol requires at least one parameter :-)
+                    final int preset = Integer.parseInt(tmp[1]);
+                    if(preset != selectedPreset) {
+                        displayPreset(preset);
+                    }
+                }
+                else if(tmp[0].equals("CHANGE_BRIGHTNESS"))
+                {
+                    //FIXME there is no way to update the controllers sliders since they dont have motors :(
+                }
+                else if(tmp[0].equals("GENERATOR_SPEED"))
+                {
+                    //FIXME there is no way to update the controllers sliders since they dont have motors :(
+                }
+            }
+        }
+    }
 }
